de81f39f54aa7ef7d9f9cf7e0569fea2eff6fbb4
Yekuuun <yekuuun@proton.me>
2025-10-14 16:14:54 +0200

    UPDATE : base update adding FactSharp to WebApp demo

--- FactSharp.WebApp/Program.cs
+++ FactSharp.WebApp/Program.cs
@@ -1,9 +1,47 @@
+using FactSharp;
+
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+var handler = new HttpClientHandler
+{
+    UseProxy = true,
+    // Autres configurations de proxy
+};
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+builder.Services.AddControllers();
+
+/*
+ * CORS POLICY => MUST BE CONFIGURED FOR YOUR OWN CAS => THIS IS A SAMPLE DEMO.
+ */
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policy =>
+        {
+            //authorize access from api gateway
+            policy.AllowAnyOrigin();
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+            policy.AllowCredentials();
+        });
+});
+
+//----------------------------------------------------
+//┌────────────────────────────────────┐
+//│ WEFACT API CONFIGURATION. 
+//└────────────────────────────────────┘ 
+//----------------------------------------------------
+builder.Services.AddWeFactApi(options =>
+{
+    options.ApiKey = Environment.GetEnvironmentVariable("WE_FACT") ?? string.Empty;
+});
 
 var app = builder.Build();
 
@@ -14,31 +52,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
