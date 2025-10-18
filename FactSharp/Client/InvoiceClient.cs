c40dc96b682bf61b4c78fb3dba1c88f78649dd32
Yekuuun <yekuuun@proton.me>
2025-10-18 16:16:01 +0200

    UPDATE : base update adding comments

--- FactSharp/Client/InvoiceClient.cs
+++ FactSharp/Client/InvoiceClient.cs
@@ -13,6 +13,11 @@ public class InvoiceClient(string apiKey, HttpClient? httpClient = null) : BaseC
     /// <returns></returns>
     public async Task<InvoiceResponse> GetInvoiceByCodeAsync(string invoiceCode) => await this.PostAsync<InvoiceResponse>(new InvoiceRequestByCode(){Controller = "invoice", Action = "show", InvoiceCode = invoiceCode});
 
+    /// <summary>
+    /// Retrieve a single invoice using it's unique id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public async Task<InvoiceResponse> GetInvoiceByIdAsync(int id) => await this.PostAsync<InvoiceResponse>(new InvoiceRequestById() { Controller = "invoice", Action = "show", Identifier = id });
 
     /// <summary>
